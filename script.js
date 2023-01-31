5018b4483b40e94ec45092fc953ef4f1acb13d62
muratdrmz <mrtdrmz@windowslive.com>
2023-01-31 16:43:26 +0100

    e

--- script.js
+++ script.js
@@ -5,5 +5,4 @@ window.addEventListener('scroll',updateImage);
 function updateImage(){
  bgImageEl.style.opacity = 1 - window.pageYOffset / 900;
  bgImageEl.style.backgroundSize = 160 - window.pageYOffset / 10 + "%";
- console.log(window.pageYOffset,160-window.pageYOffset/12);
 }
